88428045d9efa750f508b57d1d4bb09533be061a
Julien Puydt <julien.puydt@laposte.net>
2017-07-11 09:50:24 +0200

    decoupage_pdf.py: lignes moins longues et usage de os.path.join

--- utils/decoupage_pdf.py
+++ utils/decoupage_pdf.py
@@ -6,28 +6,39 @@ from parse import parse
 from parse import compile
 
 
-def decoup(sourc, dest): # source = fichier -- dest = dossier
-	regex = compile('{}Dossier n°{id:d}{}Page {page:d}') # compilation de la requête
-													# pour gagner du temps !
+def decoup(sourc, dest):
+        "sourc: fichier source"
+        "dest: dossier destination"
+        # précompilation de la requête pour gagner en vitesse
+	regex = compile('{}Dossier n°{id:d}{}Page {page:d}')
 	pdfFileObj = open(sourc, 'rb')
 	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 	page_deb = -1
 	id_cand = -1
-	pdfWriter = PyPDF2.PdfFileWriter() # pour le candidat -1 (plutôt qu'un test)
-	
-	for page in range(0,pdfReader.numPages):
-		pageObj = pdfReader.getPage(page)		# on récupère la page courante
-		txt = pageObj.extractText()				# on récupère le texte de la page
-		res = regex.parse(txt)					# récupération n° dossier et Page
+        # pour le candidat -1, au lieu de faire un cas particulier
+	pdfWriter = PyPDF2.PdfFileWriter()
+
+	for page in range(pdfReader.numPages):
+                # récupération de la page courante
+		pageObj = pdfReader.getPage(page)
+                # puis de son texte brut
+		txt = pageObj.extractText()
+                # et enfin, numéro de dossier et page
+		res = regex.parse(txt)
 		if res or page == pdfReader.numPages-1:
-			if id_cand != res['id'] or page == pdfReader.numPages-1:	# changement de candidat? fin ?
-				nom = dest+'/docs_{}.pdf'.format(id_cand)
+                        # est-ce un changement de candidat?
+			if (id_cand != res['id']
+                            or page == pdfReader.numPages-1):
+				nom = os.path.join (dest, 'docs_{}.pdf'.format(id_cand))
 				pdfOutputFile = open(nom, 'wb')
-				if page == pdfReader.numPages-1: pdfWriter.addPage(pageObj) # sinon il en manque un bout..
-				pdfWriter.write(pdfOutputFile) # Super, ça écrase tout fichier existant !!
+                                # sinon il en manque un bout
+				if page == pdfReader.numPages-1:
+                                        pdfWriter.addPage(pageObj)
+                                # écrasement de tout fichier existant!!
+				pdfWriter.write(pdfOutputFile)
 				pdfOutputFile.close()
 				# réinitialisations
 				pdfWriter = PyPDF2.PdfFileWriter()
 				id_cand = res['id']
 			pdfWriter.addPage(pageObj)
-	os.remove(dest+'/docs_-1.pdf')	
+	os.remove(os.path.join(dest, 'docs_-1.pdf'))
